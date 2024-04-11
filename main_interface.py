3df0599275c1aedbe389284a7e710d225eb7c125
ychristino <ychristino@gmail.com>
2024-04-10 21:39:18 -0300

    IDK

--- main_interface.py
+++ main_interface.py
@@ -1,6 +1,5 @@
+import webbrowser
 from concurrent.futures import ThreadPoolExecutor, as_completed
-from flask import Flask
-import sys
 
 
 def run_flask_app(app, port):
@@ -14,6 +13,7 @@ if __name__ == "__main__":
     api_port = 5000
     interface_port = 5001
 
+    webbrowser.open("http://127.0.0.1:5001")
     with ThreadPoolExecutor(max_workers=2) as executor:
         future_api = executor.submit(run_flask_app, app_api, api_port)
         future_interface = executor.submit(run_flask_app, app_interface, interface_port)
