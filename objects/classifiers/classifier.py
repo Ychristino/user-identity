26fe8a50bc29aebc4d418330ab160faa0392d3ba
ychristino <ychristino@gmail.com>
2024-04-13 19:50:32 -0300

    Adjust split time

--- objects/classifiers/classifier.py
+++ objects/classifiers/classifier.py
@@ -54,7 +54,7 @@ class Classifier(ABC):
                             merge_control: int,
                             activity: Activity = None
                             ):
-        list_mouse_movement_data, list_mouse_click_data = read_mouse_file(mouse_file_path=mouse_file_path, activity=activity, split_data_every_n_seconds=1)
+        list_mouse_movement_data, list_mouse_click_data = read_mouse_file(mouse_file_path=mouse_file_path, activity=activity, split_data_every_n_seconds=15)
 
         for index in range(len(list_mouse_movement_data)):
             self.mouse_analyses.mouse_movement_data = list_mouse_movement_data[index]
@@ -85,7 +85,7 @@ class Classifier(ABC):
                                merge_control: int,
                                activity: Activity = None
                                ):
-        list_keyboard_press_data, list_keyboard_release_data = read_keyboard_file(keyboard_file_path, activity=activity, split_data_every_n_seconds=1)
+        list_keyboard_press_data, list_keyboard_release_data = read_keyboard_file(keyboard_file_path, activity=activity, split_data_every_n_seconds=15)
         for index in range(len(list_keyboard_press_data)):
             if len(list_keyboard_release_data) > index:
                 self.keyboard_analyses.keyboard_press_data = list_keyboard_press_data[index]
